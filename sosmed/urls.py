bf46a47e22b8c9cffafe205de631fca08ef165e7
Fitri <230601110036@student.uin-malang.ac.id>
2026-05-08 12:54:28 +0700

    connect update delete buttons

--- sosmed/urls.py
+++ sosmed/urls.py
@@ -6,4 +6,7 @@ app_name = 'sosmed'
 urlpatterns = [
     path('create/', views.create, name='create'),
     path('', views.list_instagram, name='list'),
+
+    path('update/<int:update_id>/', views.update, name='update'),
+    path('delete/<int:delete_id>/', views.delete, name='delete'),
 ]
