56ce88ed5b2e62b63153dd768f1dce3a6d4920d2
Fitri <230601110036@student.uin-malang.ac.id>
2026-05-08 12:41:32 +0700

    redirect homepage to sosmed

--- my_/urls.py
+++ my_/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('/admin/')),
+    path('', lambda request: redirect('/sosmed/')),
     path('sosmed/', include(('sosmed.urls', 'sosmed'), namespace='sosmed')),
     path('admin/', admin.site.urls),
 ]
